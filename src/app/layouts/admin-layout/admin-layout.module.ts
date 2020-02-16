cb51745ef2a8f805af0b668781f5b82549fb6d0e
windsolace <eugene.nwm@gmail.com>
2020-02-16 22:02:56 +0800

    Removed unnecessary modules to avoid prod compilation error. - removed maps in head

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -7,13 +7,8 @@ import { AdminLayoutRoutes } from './admin-layout.routing';
 
 import { DashboardComponent }       from '../../pages/dashboard/dashboard.component';
 import { AboutComponent }           from '../../pages/about/about.component';
-// import { UserComponent }            from '../../pages/user/user.component';
-// import { TableComponent }           from '../../pages/table/table.component';
-// import { TypographyComponent }      from '../../pages/typography/typography.component';
-// import { IconsComponent }           from '../../pages/icons/icons.component';
-// import { MapsComponent }            from '../../pages/maps/maps.component';
-// import { NotificationsComponent }   from '../../pages/notifications/notifications.component';
-// import { UpgradeComponent }         from '../../pages/upgrade/upgrade.component';
+import { MapsComponent }            from '../../pages/maps/maps.component';
+import { NotificationsComponent }   from '../../pages/notifications/notifications.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -26,14 +21,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   declarations: [
     DashboardComponent,
-    AboutComponent
-    // UserComponent,
-    // TableComponent,
-    // UpgradeComponent,
-    // TypographyComponent,
-    // IconsComponent,
-    // MapsComponent,
-    // NotificationsComponent,
+    AboutComponent,
+    MapsComponent,
+    NotificationsComponent
   ]
 })
 
